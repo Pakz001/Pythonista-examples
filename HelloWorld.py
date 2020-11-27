bc0888ea3d03b67dc2fb80fe5a0bbd05de024347
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-11-27 15:14:09 +0100

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,30 @@
+from scene import *
+import sound
+import random
+import math
+A = Action
+
+class MyScene (Scene):
+	def setup(self):
+		button_font = ('Avenir Next', 20)
+		self.title_label = LabelNode("hello world", font=button_font, color='white', position=(self.size.x/2,self.size.y/2), parent=self)
+		pass
+	
+	def did_change_size(self):
+		pass
+	
+	def update(self):
+		
+		pass
+	
+	def touch_began(self, touch):
+		pass
+	
+	def touch_moved(self, touch):
+		pass
+	
+	def touch_ended(self, touch):
+		pass
+
+if __name__ == '__main__':
+	run(MyScene(), show_fps=True)
